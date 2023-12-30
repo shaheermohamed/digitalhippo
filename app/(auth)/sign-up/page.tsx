de8af9aeffa9b9611a32e2c38372cdc0ad1bc2f0
Mohamed Shaheer <shaheer@coderythm.com>
2023-12-30 22:38:18 +0530

    feat: added the trpc and wip: in create account page, created first user

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 "use client";
 import React from "react";
 import { Icons } from "@/components/Icons";
@@ -7,7 +8,28 @@ import { ArrowRight } from "lucide-react";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import {
+  TAuthCredentialsValidator,
+  AuthCredentialsValidator,
+} from "@/lib/validators/account-credentials-validator";
+import { trpc } from "@/trpc/client";
 const page = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TAuthCredentialsValidator>({
+    resolver: zodResolver(AuthCredentialsValidator),
+  });
+
+  const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({});
+
+  const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
+    mutate({ email, password });
+  };
   return (
     <>
       <div className="conatiner relative flex pt-20 flex-col items-center justify-center lg:px-0">
@@ -27,13 +49,14 @@ const page = () => {
             </Link>
           </div>
           <div className="grid gap-6">
-            <form onSubmit={() => {}}>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div className="grid gap-2">
                 <div className="grid gap-1 py-2">
                   <Label htmlFor="email"> Email</Label>
                   <Input
+                    {...register("email")}
                     className={cn({
-                      "focus-visible:ring-red-500": true,
+                      "focus-visible:ring-red-500": errors.email,
                     })}
                     placeholder="you@example.com"
                   />
@@ -41,8 +64,10 @@ const page = () => {
                 <div className="grid gap-1 py-2">
                   <Label htmlFor="password"> Password</Label>
                   <Input
+                    {...register("password")}
+                    type="password"
                     className={cn({
-                      "focus-visible:ring-red-500": true,
+                      "focus-visible:ring-red-500": errors.password,
                     })}
                     placeholder="password"
                   />
--- payload.config.ts
+++ payload.config.ts
@@ -3,13 +3,20 @@ import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { slateEditor } from "@payloadcms/richtext-slate";
 import { webpackBundler } from "@payloadcms/bundler-webpack";
 import path from "path";
+import { Users } from "./collections/Users";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: path.resolve(__dirname, "./env"),
+});
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [],
+  collections: [Users],
   routes: {
     admin: "/sell",
   },
   admin: {
+    user: "users",
     bundler: webpackBundler(),
     meta: {
       titleSuffix: "- DigitalHippo",
@@ -25,6 +32,6 @@ export default buildConfig({
     url: process.env.MONGODB_URL!,
   }),
   typescript: {
-    outputFile: path.resolve(__dirname,'payload-types.ts'),
+    outputFile: path.resolve(__dirname, "payload-types.ts"),
   },
 });
